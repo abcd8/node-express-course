41ced0a9d649aa5cf3fd183f0cebeebaf0bf7d29
abcd8 <brianbui.n9@gmail.com>
2020-04-04 19:17:03 +0700

    add login POST route

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = 5000;
+
+app.use(bodyParser.json());
 
 const mockUserData = [
 	{name:'Mark'},
@@ -13,7 +16,7 @@ app.get('/users', (req, res) => {
     success: true,
     message: 'Successfully get users. NICE!',
     users: mockUserData
-  })
+  });
 });
 
 app.get('/users/:id', (req, res) => {
@@ -22,7 +25,25 @@ app.get('/users/:id', (req, res) => {
 		success: true,
 		message: 'Got one user',
 		user: req.params.id
-	})
+	});
+});
+
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+  const mockUsername = 'abcd8';
+  const mockPassword = '123456';
+  if (mockUsername === username && mockPassword === password) {
+    res.json({
+      success: true,
+      message: 'Password and Username match!',
+      token: 'Encrypted token goes here'
+    });
+  } else {
+    res.json({
+      success: false,
+      message: 'Password and Username do not match'
+   });
+  }
 })
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
